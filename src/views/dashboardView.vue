45b4efb5bb0cba61445542a3108aabd1e0815beb
Tita <finaritratita@gmail.com>
2024-08-28 12:10:18 +0300

    dashboard page

--- src/views/dashboardView.vue
+++ src/views/dashboardView.vue
@@ -2,12 +2,37 @@
 import Layout from '@/layouts/layout.vue';
 </script>
 <template>
-<Layout>
-    <div>
-        <div class=" flex justify-center items-center border rounded-md w-[250px] h-[115px]">
-            <p> Create </p>
+    <Layout>
+        <div>
+            <div>
+                <div class="mb-20">
+                    <p class="mb-4">Workspace</p>
+                    <div class="flex flex-wrap gap-4 items-center text-white font-semibold">
+                        <div
+                            class="flex justify-center items-center rounded-md w-[250px] h-[115px] bg-[url('https://img.freepik.com/photos-gratuite/sommet-montagne-enneige-sous-ia-generative-majeste-galaxie-etoilee_188544-9650.jpg?size=626&ext=jpg&ga=GA1.1.1936261018.1724333182&semt=ais_hybrid')]">
+                            <p>My project</p>
+                        </div>
+                        <div
+                            class="flex justify-center items-center border rounded-md w-[250px] h-[115px] bg-[url('https://img.freepik.com/photos-gratuite/sommet-montagne-enneige-sous-ia-generative-majeste-galaxie-etoilee_188544-9650.jpg?size=626&ext=jpg&ga=GA1.1.1936261018.1724333182&semt=ais_hybrid')]">
+                            <p>Create</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <p class="mb-4">Guest Workspaces</p>
+                    <div class="flex flex-wrap gap-4 text-white font-semibold">
+                        <div
+                            class="flex justify-center items-center border rounded-md w-[250px] h-[115px] bg-[url('https://img.freepik.com/photos-gratuite/sommet-montagne-enneige-sous-ia-generative-majeste-galaxie-etoilee_188544-9650.jpg?size=626&ext=jpg&ga=GA1.1.1936261018.1724333182&semt=ais_hybrid')]">
+                            <p>simplimmo</p>
+                        </div>
+                        <div
+                            class="flex justify-center items-center border rounded-md w-[250px] h-[115px] bg-[url('https://img.freepik.com/photos-gratuite/sommet-montagne-enneige-sous-ia-generative-majeste-galaxie-etoilee_188544-9650.jpg?size=626&ext=jpg&ga=GA1.1.1936261018.1724333182&semt=ais_hybrid')]">
+                            <p>Port folio</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-</Layout>
+    </Layout>
 </template>
 <style scoped></style>
