c45497135b4ec68f57efaab44dab3ea53025798a
tody <andrianarivelotodisoa09@gmail.com>
2024-09-01 11:03:04 -0800

    composantv

--- src/views/composantView.vue
+++ src/views/composantView.vue
@@ -2,43 +2,9 @@
     <div>
         <List/>
     </div>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    <div>
-        <Card/>
-    </div>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
 </template>
 <script setup>
 import List from '@/components/List.vue';
-import Card from '@/components/Card.vue';
 
 
 </script>
