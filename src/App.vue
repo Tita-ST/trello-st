d1c7554907d8ecc708db2986c9caf053adb4ee89
tody2 <andrianarivelotodisoa09@gmail.com>
2024-09-01 08:05:38 +0200

    rectif

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { RouterLink, RouterView } from 'vue-router'
 // import HelloWorld from './components/HelloWorld.vue'
 import "./assets/main.css"
 //  import 'boxicons'
+
 </script>
 
 <template>
