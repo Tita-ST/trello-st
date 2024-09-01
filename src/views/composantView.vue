639aa1b53c86ef7b7a078672b8b4eacae429768e
tody2 <andrianarivelotodisoa09@gmail.com>
2024-09-01 09:03:27 +0200

    composant vue

--- src/views/composantView.vue
+++ src/views/composantView.vue
@@ -17,11 +17,19 @@
         <br>
         <br>
         <br>
+        <Card/>
+        <br>
+        <br>
+        <br>
+        <br>
+        <ButtonSwitch/>
 
         
     </div>
 </template>
 <script setup>
+import ButtonSwitch from '@/components/ButtonSwitch.vue';
+import Card from '@/components/Card.vue';
 import CheckList from '@/components/CheckList.vue';
 import List from '@/components/List.vue';
 import Modal from '@/components/Modal.vue';
