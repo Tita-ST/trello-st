08d7f9f96269d38233c03269c852c8cd6f1bf0ae
Tita <finaritratita@gmail.com>
2024-10-10 16:24:13 +0300

    checklist component

--- src/components/CheckList.vue
+++ src/components/CheckList.vue
@@ -12,7 +12,7 @@ import { ref } from 'vue';
             </div>
 
 
-            <div   class="flex justify-between items-center border-b p-3 border mb-1">
+            <div class="flex justify-between items-center border-b p-3 border mb-1">
                 <div class="flex items-center  ">
                     <input type="checkbox" id="example"
                         class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
@@ -20,29 +20,16 @@ import { ref } from 'vue';
                 </div>
                 <Icon icon="mingcute:user-add-fill" />
             </div>
-            <div   class="flex justify-between items-center border-b p-3 border mb-1">
-                <div class="flex items-center  ">
-                    <input type="checkbox" id="example"
-                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-                    <label for="example" class="ml-2 text-sm font-medium text-gray-900">Option 1</label>
-                </div>
-                <Icon icon="mingcute:user-add-fill" />
-            </div> <div   class="flex justify-between items-center border-b p-3 border mb-1">
-                <div class="flex items-center  ">
-                    <input type="checkbox" id="example"
-                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-                    <label for="example" class="ml-2 text-sm font-medium text-gray-900">Option 1</label>
-                </div>
-                <Icon icon="mingcute:user-add-fill" />
-            </div>
+          
             
-            
+
+
 
 
             <div class=" justify-between p-3 flex items-center border-t ">
-                <Icon  icon="gala:add" />
+                <Icon icon="gala:add" />
                 <input class="!border-none w-[92%] ml-2" type="text" placeholder="New element of checklist">
-                <Icon  icon="mingcute:user-add-fill" />
+                <Icon icon="mingcute:user-add-fill" />
             </div>
         </div>
     </div>
