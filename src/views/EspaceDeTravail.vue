996fad1f615a2da34a76cbbed6fe8ba08c712cec
Tita <finaritratita@gmail.com>
2024-10-12 13:22:28 +0300

    dashboard espace de travail

---
+++ src/views/EspaceDeTravail.vue
@@ -0,0 +1,69 @@
+<template>
+    <Layout>
+        <div class="">
+            <div>
+                <div>
+                    <p class="font-bold text-center my-10">MES ESPACES DE TRAVAIL</p>
+                    <div class="flex justify-center">
+                        <div class="flex flex-wrap gap-4 items-center text-white font-semibold">
+                            <router-link to="AddCollaborateur" class="ursor-pointer flex justify-center items-center rounded-md w-[250px] h-[115px]
+                            bg-[url('https://img.freepik.com/photos-gratuite/sommet-montagne-enneige-sous-ia-generative-majeste-galaxie-etoilee_188544-9650.jpg?size=626&ext=jpg&ga=GA1.1.1936261018.1724333182&semt=ais_hybrid')]">
+                                <p> team 1</p>
+                            </router-link>
+                            <div
+                                class="cursor-pointer flex justify-center items-center border rounded-md w-[250px] h-[115px] bg-[url('https://img.freepik.com/photos-gratuite/sommet-montagne-enneige-sous-ia-generative-majeste-galaxie-etoilee_188544-9650.jpg?size=626&ext=jpg&ga=GA1.1.1936261018.1724333182&semt=ais_hybrid')]">
+                                <p>team 2</p>
+                            </div>
+                            <div
+                                class="cursor-pointer flex justify-center items-center border rounded-md w-[250px] h-[115px] bg-[url('https://img.freepik.com/photos-gratuite/sommet-montagne-enneige-sous-ia-generative-majeste-galaxie-etoilee_188544-9650.jpg?size=626&ext=jpg&ga=GA1.1.1936261018.1724333182&semt=ais_hybrid')]">
+                                <p>team 3</p>
+                            </div>
+                            <div
+                                class="cursor-pointer flex justify-center items-center border rounded-md w-[250px] h-[115px] bg-[url('https://img.freepik.com/photos-gratuite/sommet-montagne-enneige-sous-ia-generative-majeste-galaxie-etoilee_188544-9650.jpg?size=626&ext=jpg&ga=GA1.1.1936261018.1724333182&semt=ais_hybrid')]">
+                                <p>Create</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <p class="font-bold text-center my-10">MES ESPACES DE TRAVAIL EN TANT QU'INVITÉ</p>
+                <div class="flex justify-center">
+                    <div class="flex flex-wrap gap-4 items-center text-white font-semibold">
+                        <div
+                            class="cursor-pointer flex justify-center items-center rounded-md w-[250px] h-[115px] bg-[url('https://img.freepik.com/photos-gratuite/sommet-montagne-enneige-sous-ia-generative-majeste-galaxie-etoilee_188544-9650.jpg?size=626&ext=jpg&ga=GA1.1.1936261018.1724333182&semt=ais_hybrid')]">
+                            <p> project</p>
+                        </div>
+                        <div
+                            class="cursor-pointer flex justify-center items-center border rounded-md w-[250px] h-[115px] bg-[url('https://img.freepik.com/photos-gratuite/sommet-montagne-enneige-sous-ia-generative-majeste-galaxie-etoilee_188544-9650.jpg?size=626&ext=jpg&ga=GA1.1.1936261018.1724333182&semt=ais_hybrid')]">
+                            <p>projet</p>
+                        </div>
+                        <div
+                            class="cursor-pointer flex justify-center items-center border rounded-md w-[250px] h-[115px] bg-[url('https://img.freepik.com/photos-gratuite/sommet-montagne-enneige-sous-ia-generative-majeste-galaxie-etoilee_188544-9650.jpg?size=626&ext=jpg&ga=GA1.1.1936261018.1724333182&semt=ais_hybrid')]">
+                            <p>projet</p>
+                        </div>
+                        <div
+                            class="cursor-pointer flex justify-center items-center border rounded-md w-[250px] h-[115px] bg-[url('https://img.freepik.com/photos-gratuite/sommet-montagne-enneige-sous-ia-generative-majeste-galaxie-etoilee_188544-9650.jpg?size=626&ext=jpg&ga=GA1.1.1936261018.1724333182&semt=ais_hybrid')]">
+                            <p>projet </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="mt-10 text-center">
+                <p class="font-bold"> Qu'est ce qu'un espace de travail?</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate assumenda porro sed, voluptatum
+                    eligendi accusantium doloribus ea minima eaque atque ducimus incidunt magnam illo suscipit nihil
+                    quis
+                    reprehenderit necessitatibus, ipsum sint pariatur debitis aut reiciendis. Ex, neque alias ipsam
+                    deserunt
+                    nulla expedita autem voluptates, nostrum a earum labore laborum praesentium!</p>
+            </div>
+        </div>
+    </Layout>
+</template>
+<script setup>
+import Layout from '@/layouts/layout.vue';
+
+
+</script>
+<style></style>
