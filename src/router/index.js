60cd8df01ab01a3f0db1a288c4da3a2357ec0965
Tita <finaritratita@gmail.com>
2024-10-12 13:21:45 +0300

    ajouts router

--- src/router/index.js
+++ src/router/index.js
@@ -74,6 +74,23 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/TableauxView.vue')
     },
+    {
+      path: '/AddCollaborateur',
+      name: 'AddCollaborateur',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AddCollaborateurView.vue')
+    },
+    {
+      path: '/EspaceDeTravail',
+      name: 'EspaceDeTravail',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/EspaceDeTravail.vue')
+    },
+
 
   ]
 })
